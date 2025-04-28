50e6b7854cc376e5e31f812e6e3c250cdfa4a243
Cuong <ntancuong0211@gmail.com>
2025-04-28 18:51:38 +0700

    layout web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import ThemeProvider from "@/theme";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-[linear-gradient(to_right,_#e2e2e2,_#c9d6ff)] dark:bg-[linear-gradient(to_right,_#121212,_#121212)] text-[#1F2937] dark:text-[#FFFFFF]">
+      <body className="bg-[linear-gradient(to_right,_#e2e2e2,_#c9d6ff)] dark:bg-[linear-gradient(to_right,_#171717,_#171717)] dark:text-[#FFFFFF]">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
