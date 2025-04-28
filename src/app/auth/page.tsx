ce71f824b31882f20dc55d6e0c967ea3481a00e4
Cuong <ntancuong0211@gmail.com>
2025-04-28 18:51:18 +0700

    Login page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,8 +3,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-import { TextField, Button, Card, CardContent, Typography, IconButton, InputAdornment, Divider, Box, Snackbar, Alert } from "@mui/material";
-import { Visibility, VisibilityOff, Email, Person, Lock, Facebook, Google, GitHub } from "@mui/icons-material";
+import { TextField, Button, Card, CardContent, Typography, IconButton, InputAdornment, CircularProgress, Divider, Box, Snackbar, Alert } from "@mui/material";
+import { Visibility, VisibilityOff, AlternateEmail, LockOutlined, Facebook, Google, GitHub } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import ThemeToggleSwitch from "@/components/ThemeToggleButton";
@@ -28,6 +28,7 @@ export default function AuthPage() {
     message: "",
     severity: "success",
   });
+  const [isLoading, setIsLoading] = useState(false); // State to manage loading
 
   // Event handlers
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,9 +44,12 @@ export default function AuthPage() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
+    // Sign up logic
     if (isSignUp) {
       if (formData.password !== formData.confirmPassword) {
         setErrorMessage("Password does not match.");
+        setIsLoading(false);
         return;
       }
       setErrorMessage("");
@@ -65,8 +69,11 @@ export default function AuthPage() {
           message: "Failed to register. Please try again.",
           severity: "error",
         });
+      } finally {
+        setIsLoading(false);
       }
     } else {
+      // Sign in logic
       try {
         const result = await signIn("credentials", {
           redirect: false,
@@ -75,14 +82,14 @@ export default function AuthPage() {
         });
 
         if (result?.error) {
-          // Nếu login thất bại
+          // If login fails
           setSnackbar({
             open: true,
             message: result.error || "Failed to login. Please try again.",
             severity: "error",
           });
         } else {
-          // Nếu login thành công
+          // If login succeeds
           router.push("/");
         }
       } catch (error: any) {
@@ -92,6 +99,8 @@ export default function AuthPage() {
           message: error.response?.data?.error || "Failed to login. Please try again.",
           severity: "error",
         });
+      } finally {
+        setIsLoading(false);
       }
     }
   };
@@ -100,13 +109,39 @@ export default function AuthPage() {
     setSnackbar({ ...snackbar, open: false });
   };
 
+  const commonTextFieldStyles = {
+    "& .MuiOutlinedInput-root": {
+      borderRadius: "10px",
+      paddingTop: "0.2rem",
+      paddingBottom: "0.2rem",
+      ".dark &": {
+        backgroundColor: "#2b2b2b",
+      },
+    },
+    "& .MuiOutlinedInput-input": {
+      "&::placeholder": {
+        ".dark &": {
+          color: "#d1d5db",
+        },
+      },
+      ".dark &": {
+        caretColor: "#ffffff",
+        color: "#ffffff",
+      },
+    },
+  };
+
   return (
     <>
-      <Box className="dark:bg-[linear-gradient(to_right,_#e2e2e2,_#c9d6ff)] flex items-center justify-center min-h-screen">
-        <Card className="w-full max-w-sm" sx={{ borderRadius: 3, boxShadow: 10 }}>
-          <CardContent className="p-8">
+      <Box className="flex items-center justify-center min-h-screen">
+        <ThemeToggleSwitch />
+        <Card
+          className="w-full max-w-sm"
+          sx={{ borderRadius: 5, boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.3)", ".dark &": { backgroundColor: "#1f1f1f" } }}
+        >
+          <CardContent className="p-8 dark:bg-[#1f1f1f]">
             {/* Header */}
-            <Typography variant="h5" component="h1" className="text-center" sx={{ fontWeight: "bold", marginBottom: 3 }}>
+            <Typography variant="h5" component="h1" className="text-center dark:text-[#f1f1f1]" sx={{ fontWeight: "bold", marginBottom: 3 }}>
               {isSignUp ? "Sign Up" : "Sign In"}
             </Typography>
 
@@ -125,16 +160,7 @@ export default function AuthPage() {
                     size="small"
                     margin="normal"
                     required
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <Person sx={{ color: "#6c5ce7" }} />
-                        </InputAdornment>
-                      ),
-                    }}
-                    sx={{
-                      borderRadius: 20,
-                    }}
+                    sx={commonTextFieldStyles}
                   />
 
                   {/* Last Name Input */}
@@ -148,13 +174,7 @@ export default function AuthPage() {
                     size="small"
                     margin="normal"
                     required
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <Person sx={{ color: "#6c5ce7" }} />
-                        </InputAdornment>
-                      ),
-                    }}
+                    sx={commonTextFieldStyles}
                   />
                 </Box>
               )}
@@ -174,10 +194,11 @@ export default function AuthPage() {
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
-                      <Email sx={{ color: "#6c5ce7" }} />
+                      <AlternateEmail sx={{ color: "black", ".dark &": { color: "white" }, marginRight: 1 }} />
                     </InputAdornment>
                   ),
                 }}
+                sx={commonTextFieldStyles}
               />
 
               {/* Password Input */}
@@ -187,7 +208,7 @@ export default function AuthPage() {
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
-                placeholder="Enter your Password"
+                placeholder="Password"
                 variant="outlined"
                 size="small"
                 margin="normal"
@@ -195,17 +216,18 @@ export default function AuthPage() {
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
-                      <Lock sx={{ color: "#6c5ce7" }} />
+                      <LockOutlined sx={{ color: "black", ".dark &": { color: "white" }, marginRight: 1 }} />
                     </InputAdornment>
                   ),
                   endAdornment: (
                     <InputAdornment position="end">
                       <IconButton onClick={() => setShowPassword(!showPassword)} edge="end" aria-label="toggle password visibility">
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                        {showPassword ? <VisibilityOff sx={{ color: "black", ".dark &": { color: "white" } }} /> : <Visibility sx={{ color: "black", ".dark &": { color: "white" } }} />}
                       </IconButton>
                     </InputAdornment>
                   ),
                 }}
+                sx={commonTextFieldStyles}
               />
 
               {isSignUp && (
@@ -225,10 +247,11 @@ export default function AuthPage() {
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">
-                        <Lock sx={{ color: "#6c5ce7" }} />
+                        <LockOutlined sx={{ color: "black", ".dark &": { color: "white" }, marginRight: 1 }} />
                       </InputAdornment>
                     ),
                   }}
+                  sx={commonTextFieldStyles}
                 />
               )}
 
@@ -247,24 +270,34 @@ export default function AuthPage() {
                   type="submit"
                   variant="contained"
                   sx={{
-                    px: 10,
                     backgroundColor: "#1F2937",
+                    width: "50%",
                     transition: "background-color 0.3s ease",
                     color: "#ffffff",
                     "&:hover": {
                       backgroundColor: "#111827",
                     },
+                    "&.Mui-disabled": {
+                      backgroundColor: "#111827",
+                    },
+                    ".dark &": {
+                      backgroundColor: "#2d79f3",
+                      "&:hover": {
+                        backgroundColor: "#1e40af",
+                      },
+                    },
                   }}
                   disableElevation
+                  disabled={isLoading}
                 >
-                  {isSignUp ? "Sign Up" : "Login"}
+                  {isLoading ? <CircularProgress size={25} sx={{ color: "#ffffff" }} /> : isSignUp ? "Sign Up" : "Login"}
                 </Button>
               </Box>
             </Box>
 
             {/* Toggle Sign In/Sign Up */}
             <Box className="mt-6 text-center">
-              <Typography variant="body2" className="text-gray-700">
+              <Typography variant="body2" className="text-gray-700 dark:text-[#f1f1f1]">
                 {isSignUp ? "Already have an account? " : "Don’t have an account? "}
                 <Button onClick={() => setIsSignUp(!isSignUp)} className="text-blue-600 hover:underline" sx={{ textTransform: "none", padding: 0 }}>
                   {isSignUp ? "Sign In" : "Sign Up"}
@@ -276,19 +309,55 @@ export default function AuthPage() {
             <Box className="mt-8 mb-2">
               <Box className="relative flex items-center justify-center">
                 <Divider className="w-full" />
-                <Typography variant="body2" className="absolute px-4 bg-white text-gray-600">
+                <Typography variant="body2" className="absolute px-4 bg-white dark:bg-[#1f1f1f] text-gray-600 dark:text-gray-400">
                   or connect with
                 </Typography>
               </Box>
 
               <Box className="flex justify-center mt-6 space-x-4">
-                <IconButton sx={{ color: "#155dfc" }} size="large" aria-label="login with Facebook">
+                <IconButton
+                  sx={{
+                    color: "#155dfc",
+                    ".dark &": {
+                      color: "#4c6ef5",
+                      "&:hover": {
+                        backgroundColor: "#333333",
+                      },
+                    },
+                  }}
+                  size="large"
+                  aria-label="login with Facebook"
+                >
                   <Facebook />
                 </IconButton>
-                <IconButton sx={{ color: "#dc2626" }} size="large" aria-label="login with Google">
+                <IconButton
+                  sx={{
+                    color: "#dc2626",
+                    ".dark &": {
+                      color: "#f87171",
+                      "&:hover": {
+                        backgroundColor: "#333333",
+                      },
+                    },
+                  }}
+                  size="large"
+                  aria-label="login with Google"
+                >
                   <Google />
                 </IconButton>
-                <IconButton sx={{ color: "#1e2939" }} size="large" aria-label="login with GitHub">
+                <IconButton
+                  sx={{
+                    color: "#1e2939",
+                    ".dark &": {
+                      color: "#334155",
+                      "&:hover": {
+                        backgroundColor: "#333333",
+                      },
+                    },
+                  }}
+                  size="large"
+                  aria-label="login with GitHub"
+                >
                   <GitHub />
                 </IconButton>
               </Box>
